5953a913ded06fdbbd7ab5e2c28b94289c300b4a
Oskar Wieczorek <oziw57@gmail.com>
2026-05-13 08:33:04 +0100

    update test file

--- haskell_test.hs
+++ haskell_test.hs
@@ -1,24 +1,17 @@
-```haskell
 module ExamRevision where
 
 import Data.Char (isLower)
 
---------------------------------------------------
--- ALGEBRAIC DATA TYPES
---------------------------------------------------
-
 data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun
     deriving (Eq, Ord, Show)
 
+-- #1 algebraic data types
 isWeekend :: Day -> Bool
 isWeekend Sat = True
 isWeekend Sun = True
 isWeekend _   = False
 
---------------------------------------------------
--- GUARDS
---------------------------------------------------
-
+-- #2 guards
 grade :: Int -> Char
 grade mark
     | mark >= 70 = 'A'
@@ -27,96 +20,85 @@ grade mark
     | mark >= 40 = 'D'
     | otherwise  = 'F'
 
+-- #3 guards
 absolute :: Int -> Int
 absolute x
     | x < 0     = -x
     | otherwise = x
 
---------------------------------------------------
--- PATTERN MATCHING
---------------------------------------------------
-
+-- #4 pattern matching
 headPlusOne :: [Int] -> Int
 headPlusOne []    = -1
 headPlusOne (x:_) = x + 1
 
+-- #5 pattern matching
 isEmpty :: [a] -> Bool
 isEmpty [] = True
 isEmpty _  = False
 
---------------------------------------------------
--- RECURSION
---------------------------------------------------
-
+-- #6 recursion
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
+-- #7 recursion
 sumNumbers :: Int -> Int
 sumNumbers 0 = 0
 sumNumbers n = n + sumNumbers (n - 1)
 
---------------------------------------------------
--- LIST RECURSION
---------------------------------------------------
-
+-- #8 recursion lists
 listLength :: [a] -> Int
 listLength []     = 0
 listLength (_:xs) = 1 + listLength xs
 
+-- #9 recursion lists
 sumList :: [Int] -> Int
 sumList []     = 0
 sumList (x:xs) = x + sumList xs
 
+-- #10 recursion + guards
 removeAll :: Int -> [Int] -> [Int]
 removeAll _ [] = []
 removeAll n (x:xs)
     | n == x    = removeAll n xs
     | otherwise = x : removeAll n xs
 
+-- #11 recursion + guards
 countEven :: [Int] -> Int
 countEven [] = 0
 countEven (x:xs)
     | even x    = 1 + countEven xs
     | otherwise = countEven xs
 
---------------------------------------------------
--- LIST COMPREHENSIONS
---------------------------------------------------
-
+-- #12 list comprehension
 firstSquares :: Int -> [Int]
 firstSquares n = [x^2 | x <- [1..n]]
 
+-- #13 list comprehension
 onlyLowerCase :: String -> String
 onlyLowerCase xs = [x | x <- xs, isLower x]
 
---------------------------------------------------
--- HIGHER ORDER FUNCTIONS
---------------------------------------------------
-
+-- #14 higher order map
 mult10 :: [Int] -> [Int]
 mult10 = map (*10)
 
+-- #15 higher order filter
 onlyPositive :: [Int] -> [Int]
 onlyPositive = filter (>0)
 
+-- #16 foldr
 sumUsingFold :: [Int] -> Int
 sumUsingFold = foldr (+) 0
 
---------------------------------------------------
--- TUPLES
---------------------------------------------------
-
+-- #17 tuples
 sumDifference :: Int -> Int -> (Int, Int)
 sumDifference x y = (x + y, x - y)
 
+-- #18 tuples
 fstValue :: (a,b) -> a
 fstValue (x,_) = x
 
---------------------------------------------------
--- CUSTOM TYPES WITH DATA
---------------------------------------------------
-
+-- #19 algebraic data types
 data StudentMark = Student String Int
     deriving (Show)
 
@@ -128,56 +110,40 @@ studentGrade (Student _ mark)
     | mark >= 40 = 'D'
     | otherwise  = 'F'
 
---------------------------------------------------
--- TREES
---------------------------------------------------
-
+-- #20 tree recursion
 data Tree = Null
           | Node Int Tree Tree
           deriving (Show)
-
-exampleTree :: Tree
-exampleTree =
-    Node 10
-        (Node 5
-            (Node 2 Null Null)
-            (Node 7 Null Null))
-        (Node 15
-            Null
-            (Node 20 Null Null))
-
---------------------------------------------------
--- TREE RECURSION
---------------------------------------------------
 
 height :: Tree -> Int
 height Null = 0
 height (Node _ l r) = 1 + max (height l) (height r)
 
+-- #21 tree recursion
 sumTree :: Tree -> Int
 sumTree Null = 0
 sumTree (Node n l r) = n + sumTree l + sumTree r
 
+-- #22 tree recursion
 numberOfNodes :: Tree -> Int
 numberOfNodes Null = 0
 numberOfNodes (Node _ l r) =
     1 + numberOfNodes l + numberOfNodes r
 
+-- #23 tree recursion
 isMember :: Int -> Tree -> Bool
 isMember _ Null = False
 isMember n (Node m l r)
     | n == m    = True
     | otherwise = isMember n l || isMember n r
 
+-- #24 tree recursion
 inOrder :: Tree -> [Int]
 inOrder Null = []
 inOrder (Node n l r) =
     inOrder l ++ [n] ++ inOrder r
 
---------------------------------------------------
--- BINARY SEARCH TREE INSERTION
---------------------------------------------------
-
+-- #25 binary search tree
 insertTree :: Int -> Tree -> Tree
 insertTree n Null = Node n Null Null
 insertTree n (Node m l r)
@@ -185,51 +151,41 @@ insertTree n (Node m l r)
     | n > m     = Node m l (insertTree n r)
     | otherwise = Node m l r
 
+-- #26 binary search tree
 listToTree :: [Int] -> Tree
 listToTree = foldr insertTree Null
 
---------------------------------------------------
--- FUNCTION COMPOSITION
---------------------------------------------------
-
+-- #27 function composition
 squareRootsPositive :: [Float] -> [Float]
 squareRootsPositive = map sqrt . filter (>=0)
 
---------------------------------------------------
--- LAMBDA FUNCTIONS
---------------------------------------------------
-
+-- #28 lambda functions
 zeroToTen :: [Int] -> [Int]
 zeroToTen = filter (\x -> x >= 0 && x <= 10)
 
---------------------------------------------------
--- WHERE CLAUSES
---------------------------------------------------
-
+-- #29 where clauses
 circleArea :: Float -> Float
 circleArea r = pi * radiusSquared
     where
         radiusSquared = r * r
 
---------------------------------------------------
--- COMMON EXAM STYLE FUNCTIONS
---------------------------------------------------
-
+-- #30 recursion lists
 prefix :: [Int] -> [Int] -> Bool
 prefix [] _ = True
 prefix _ [] = False
 prefix (x:xs) (y:ys) =
     x == y && prefix xs ys
 
+-- #31 recursion lists
 sorted :: [Int] -> Bool
 sorted [] = True
 sorted [_] = True
 sorted (x:y:xs) =
     x <= y && sorted (y:xs)
 
+-- #32 recursion
 power :: Int -> Int -> Int
 power _ 0 = 1
 power n p
     | p < 0     = error "Negative powers not supported"
     | otherwise = n * power n (p - 1)
-```
